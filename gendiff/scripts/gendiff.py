c58d9dd7876a34da7221520a9384ff8f897d4e29
Maksim Iliukovich <m.iliukovich@corp.vk.com>
2021-10-30 02:15:51 +0300

    first step - init project & setup help message

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
