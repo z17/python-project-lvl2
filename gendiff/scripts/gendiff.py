08a7bd6445ed3ad9b16cfedae52cb97a6281667e
Maksim Iliukovich <m.iliukovich@corp.vk.com>
2021-11-17 13:31:14 +0300

    step2: added format argument

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,8 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
 
+    parser.add_argument('-f', '--format', help='set format of output')
+
     parser.parse_args()
 
 
