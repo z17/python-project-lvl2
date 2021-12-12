91f8aaaa213e68d4205cce807fb9f2b267d3b15c
Maksim Iliukovich <admin@blweb.ru>
2021-12-12 16:15:42 +0300

    default style argument

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,9 +1,9 @@
-from gendiff.formatter.render import render
+from gendiff.formatter.render import render, STYLE_STYLISH
 from gendiff.differ.loader import get_file_data
 from gendiff.differ.differ import find_diff
 
 
-def generate_diff(file_path1, file_path2, style):
+def generate_diff(file_path1, file_path2, style=STYLE_STYLISH):
     text1, extension1 = get_file_data(file_path1)
     text2, extension2 = get_file_data(file_path2)
 
